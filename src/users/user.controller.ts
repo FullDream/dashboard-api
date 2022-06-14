2d892f943e8e42171b94f31a90beb74769a1b2ae
Pavel Rybakin <pvrybakin@gmail.com>
2022-06-14 22:57:56 +0300

    add jwt

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -3,19 +3,23 @@ import { injectable, inject } from 'inversify'
 import { BaseController } from '../common/base.controller'
 import { HTTPError } from '../errors/http-error.class'
 import { ILogger } from '../logger/logger.interface'
-import { TYPES } from '../types'
+import { sign } from 'jsonwebtoken'
 import 'reflect-metadata'
+
+import { TYPES } from '../types'
 import { IUserController } from './users.controller.interface'
 import { UserLoginDto } from './dto/user-login.dto'
 import { UserRegisterDto } from './dto/user-register.dto'
-import { UserService } from './users.service'
 import { ValidateMiddleware } from '../common/validate.middleware'
+import { IConfigService } from '../config/config.service.interface'
+import { IUserService } from './users.service.interface'
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
 		@inject(TYPES.ILogger) private loggerService: ILogger,
-		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService)
 		this.bindRoutes([
@@ -43,7 +47,8 @@ export class UserController extends BaseController implements IUserController {
 		if (!result) {
 			return next(new HTTPError(401, 'ошибка авторизации', 'login'))
 		}
-		this.ok(res, {})
+		const jwt = await this.signJWT(req.body.email, this.configService.get('SECRET'))
+		this.ok(res, { jwt })
 	}
 
 	async register(
@@ -56,5 +61,24 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HTTPError(422, 'Такой пользователь уже существует'))
 		}
 		this.ok(res, { email: result.email, id: result.id })
+	}
+
+	private signJWT(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{
+					algorithm: 'HS256',
+				},
+				(error, token) => {
+					if (error) reject(error)
+					resolve(token as string)
+				},
+			)
+		})
 	}
 }
